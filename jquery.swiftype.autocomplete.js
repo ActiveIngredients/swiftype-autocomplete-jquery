d1a0ce53c490ca3859c6f5523d553bffe3066e27
Mike Zens <mzens@activeingredients.com>
2021-04-22 10:51:48 -0600

    Require.js Support for Swiftype jQuery Library

--- jquery.swiftype.autocomplete.js
+++ jquery.swiftype.autocomplete.js
@@ -1,4 +1,16 @@
-(function ($) {
+(function(factory) {
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['jquery'], factory);
+	} else if (typeof module !== 'undefined' && module.exports) {
+		// CommonJS
+		module.exports = factory(require('jquery'));
+	} else {
+		// Global
+		factory(jQuery);
+	}
+})(function ($) {
   var queryParser = function (a) {
       var i, p, b = {};
       if (a === "") {
@@ -565,5 +577,4 @@
     autocompleteContainingElement: 'body',
     widgetContainerClass: 'swiftype-widget'
   };
-
-})(jQuery);
+});
